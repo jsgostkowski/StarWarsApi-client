64572e0422cf7d70a8e397cf09f152971b22dbdf
jgostkowski <gostkowskijakub@gmail.com>
2022-12-13 21:55:29 +0100

    update fetching sercive

--- src/hooks/useFetchMovie.tsx
+++ src/hooks/useFetchMovie.tsx
@@ -1,8 +1,8 @@
 import { useQuery } from "@tanstack/react-query";
-import { fetchMovie } from "../actions";
+import { actions, fetchMovie } from "../actions";
 
 export const useFetchMovie = (id: string) => {
-  const query = useQuery(["movie", id], () => fetchMovie(id));
+  const query = useQuery(["movie", id], () => actions.fetchMovie(id));
 
   return query;
 };
